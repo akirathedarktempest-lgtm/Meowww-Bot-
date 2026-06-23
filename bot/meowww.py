6686d0ea2d112e08f0c0e60e234f96aeee9aedf4
Yu-zu-i chan! <akirathedarktempest@gmail.com>
2026-06-23 18:29:04 +0530

    Add channel selection command and database support

--- bot/meowww.py
+++ bot/meowww.py
@@ -11,16 +11,51 @@ bot=commands.Bot("$",intents=intents)
 async def on_ready():
   print("The bot is ready meoww")
 
+connect=sqlite3.connect("guilds.db")
+cursor=connect.cursor()
+cursor.execute("""CREATE TABLE IF NOT EXISTS ids(
+               guild_id integer,
+               channel_id integer)""")
+connect.commit()
+
+@bot.tree.command()
+@app_commands.checks.has_permissions(manage_guild=True)#this command will basically allow you to chose that in which channel you want the meow work, like where you will like it to say meow whenever it says anything
+async def chosechannel(interaction:discord.Interaction, channel:str):
+    if channel.endswith(">") and channel.startswith("<#"):
+        channel=channel.replace("<#","")
+        channel=channel.replace(">","")
+        channel=int(channel)
+        guildid=interaction.guild_id
+        d={"guild_id":guildid, "channel_id":channel}
+        l=[d["guild_id"],d["channel_id"]]
+        cursor.execute("INSERT INTO ids VALUES (?,?)",l)
+        cursor.execute("SELECT * FROM ids")
+        connect.commit()
+        line=cursor.fetchall()
+        print(line)
+        await interaction.response.defer(ephemeral=True)
+        await interaction.followup.send("The channel is assigned! You can try that now.")
+    else:
+        await interaction.response.defer(ephemeral=True)
+        await interaction.followup.send("You should just mention a channel to allow")
+
 @bot.event
 async def on_message(message:discord.Message):
-  if message.author==bot.user:
-    return
-  if message.channel.id==1518594138214371378:#give your channel id where you want the bot to say meow on every message you send
-    if "" in message.content:
-      channel=message.guild.get_channel(1518594138214371378)#use that channel only
-      await channel.send(f"{random.choice(meow)}")
-  await bot.proccess_commands(message)#this will allow you to use commands when it's a command call
+    if message.author==bot.user:
+        return
+    else:
+        cursor.execute("SELECT * FROM ids")
+        line=cursor.fetchall()
+        for i in line:
+            if message.guild.id==i[0]:
+                if message.channel.id==i[1]:
+                    channel=message.guild.get_channel(message.channel.id)
+                    await channel.send(f"{random.choice(meow)}")
+                    break
+                else:
+                    continue
+            else:
+                continue
+    await bot.process_commands(message)
 
 bot.run("TOKEN")
-#i have done it better, like I have made a whole embed with random images, but I am scared of copyrights of the image here, so I am not using that here, but you can look at my channel and server
-#and this may look spammy, but i have filled random, and we will try to give a cooldown, events can't make cooldown unless it is cog, but we will use sqlite3 for that later ^^
