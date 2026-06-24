5e3050f1e08132f2e7a54ae571d126cca4a1c3db
Yu-zu-i chan! <akirathedarktempest@gmail.com>
2026-06-24 17:23:19 +0530

    Integrate SQLite for channel management
    Refactor bot to use SQLite for channel assignments and remove redundant code.

--- bot/meowww.py
+++ bot/meowww.py
@@ -1,61 +1,62 @@
 import discord
 from discord.ext import commands
+import sqlite3
 import random
+from discord import app_commands
 
-meow=["meow","meow meow I like you!","meowww","hm...meow","meow meow meeeow","meow :3","meow meow meowwwww"]
+connect=sqlite3.connect("guild.db")
+cursor=connect.cursor()
+
+cursor.execute("""CREATE TABLE IF NOT EXISTS ids(
+               guild_id integer,
+               channel_id str)""")
 
 intents=discord.Intents.all()
 bot=commands.Bot("$",intents=intents)
 
 @bot.event
 async def on_ready():
-  print("The bot is ready meoww")
+    print(f"The {bot.user} is ready!")
+    await bot.tree.sync()
 
-connect=sqlite3.connect("guilds.db")
-cursor=connect.cursor()
-cursor.execute("""CREATE TABLE IF NOT EXISTS ids(
-               guild_id integer,
-               channel_id integer)""")
-connect.commit()
-
-@bot.tree.command()
-@app_commands.checks.has_permissions(manage_guild=True)#this command will basically allow you to chose that in which channel you want the meow work, like where you will like it to say meow whenever it says anything
-async def chosechannel(interaction:discord.Interaction, channel:str):
-    if channel.endswith(">") and channel.startswith("<#"):
-        channel=channel.replace("<#","")
-        channel=channel.replace(">","")
-        channel=int(channel)
-        guildid=interaction.guild_id
-        d={"guild_id":guildid, "channel_id":channel}
-        l=[d["guild_id"],d["channel_id"]]
-        cursor.execute("INSERT INTO ids VALUES (?,?)",l)
-        cursor.execute("SELECT * FROM ids")
-        connect.commit()
-        line=cursor.fetchall()
-        print(line)
-        await interaction.response.defer(ephemeral=True)
-        await interaction.followup.send("The channel is assigned! You can try that now.")
-    else:
-        await interaction.response.defer(ephemeral=True)
-        await interaction.followup.send("You should just mention a channel to allow")
-
-@bot.event
-async def on_message(message:discord.Message):
-    if message.author==bot.user:
-        return
-    else:
-        cursor.execute("SELECT * FROM ids")
-        line=cursor.fetchall()
-        for i in line:
-            if message.guild.id==i[0]:
-                if message.channel.id==i[1]:
-                    channel=message.guild.get_channel(message.channel.id)
-                    await channel.send(f"{random.choice(meow)}")
-                    break
-                else:
-                    continue
+@bot.tree.command(name="choose")
+@app_commands.checks.has_permissions(manage_guild=True)
+async def choose(interaction:discord.Interaction,channel:str):
+    await interaction.response.defer(ephemeral=True)
+    cursor.execute("SELECT * FROM ids")
+    info=cursor.fetchall()
+    for i in info:
+        if i[0]==interaction.guild.id:
+            if channel.startswith("<#") and channel.endswith(">"):
+                channel=channel.rstrip(">")
+                channel=channel.lstrip("<#")
+                try:
+                    channel=int(channel)
+                    cursor.execute(f"UPDATE ids SET channel_id=? WHERE guild_id={interaction.guild.id}",[f"{str(channel)}-"])
+                    connect.commit()
+                    return await interaction.followup.send(f"The <#{channel}> has been assigned!")
+                except ValueError as e:
+                    print(e)
+                    return await interaction.followup.send("Please send a proper channel mention only")
             else:
-                continue
-    await bot.process_commands(message)
+                return await interaction.followup.send("Please send a proper channel mention only")
+        else:
+            print("Checking line 43")
+    if channel.startswith("<#") and channel.endswith(">"):
+        channel=channel.rstrip(">")
+        channel=channel.lstrip("<#")
+        try:
+            channel=int(channel)
+            cursor.execute("INSERT INTO ids VALUES (?,?)",[interaction.guild.id,f"{channel}-"])
+            connect.commit()
+            await interaction.followup.send(f"The <#{channel}> has been assigned!")
+            return
+        except ValueError as e:
+            print(e)
+            return await interaction.followup.send("Please send a proper channel mention only")
+    else:
+        return await interaction.followup.send("Please send a proper channel mention only")
 
 bot.run("TOKEN")
+
+#rebuilding everything from scratch, but it wouldn't take much time
