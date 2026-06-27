5a81c8f75bd0ed2b4aac8c65d84651bf4b66b769
Yu-zu-i chan! <akirathedarktempest@gmail.com>
2026-06-27 17:01:40 +0530

    Implement GuildData and ChannelData functions
    Added functions to retrieve guild and channel IDs from the database.

--- bot/meowww.py
+++ bot/meowww.py
@@ -14,6 +14,21 @@ cursor.execute("""CREATE TABLE IF NOT EXISTS ids(
 intents=discord.Intents.all()
 bot=commands.Bot("$",intents=intents)
 
+def GuildData():
+    cursor.execute("SELECT guild_id FROM ids")
+    info=cursor.fetchall()
+    l=[]
+    for i in info:
+        l.append(i[0])
+    return l
+def ChannelData():
+    cursor.execute("SELECT channel_id FROM ids")
+    info=cursor.fetchall()
+    l=[]
+    for i in info:
+        l.append(i[0])
+    return l
+
 @bot.event
 async def on_ready():
     print(f"The {bot.user} is ready!")
@@ -56,7 +71,29 @@ async def choose(interaction:discord.Interaction,channel:str):
             return await interaction.followup.send("Please send a proper channel mention only")
     else:
         return await interaction.followup.send("Please send a proper channel mention only")
+    
+@bot.event
+async def on_message(message:discord.Message):
+    guild=GuildData()
+    channel=ChannelData()
+    if message.author==bot.user:
+        return
+    if message.guild.id in guild:
+        number=guild.index(message.guild.id)
+        channel_ids=channel[number]
+        channel_ids=channel_ids.split("-")
+        for i in channel_ids:
+            if message.channel.id==int(i):
+                channels=message.guild.get_channel(int(i))
+                await channels.send(random.choice(["meow","meowww","meow ;3","meow :3"]))
+                break
+            else:
+                pass
+    await bot.process_commands(message)
 
 bot.run("TOKEN")
 
 #rebuilding everything from scratch, but it wouldn't take much time
+#added two functions which will take care of making lists of guild id and channel id
+#you may think, then it can be a problem at indexing? well, most probably it shouldn't, because the indexing of GuildData and ChannelData will be same, it depends on database
+#and the guild id wouldn't have empty channels, because the guild id is only added when you are also adding its channel id, if the guild id already exist in db, channel id will be added, if not then it will be created
