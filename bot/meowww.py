034912e4a7da3e4ad0d34fb956598150661c8711
Yu-zu-i chan! <akirathedarktempest@gmail.com>
2026-07-01 16:02:18 +0530

    Refactor bot to implement tagging system and cleanup
    Refactored bot functionality by removing the old ID management and adding a new tagging system. Introduced a new command for tagging with links and improved the on_message event handling.

--- bot/meowww.py
+++ bot/meowww.py
@@ -3,94 +3,62 @@ from discord.ext import commands
 import sqlite3
 import random
 from discord import app_commands
-
-connect=sqlite3.connect("guild.db")
-cursor=connect.cursor()
-
-cursor.execute("""CREATE TABLE IF NOT EXISTS ids(
-               guild_id integer,
-               channel_id str)""")
+import requests
 
 intents=discord.Intents.all()
 bot=commands.Bot("$",intents=intents)
 
-def IDS():
-    cursor.execute("SELECT * FROM ids")
-    info=cursor.fetchall()
-    d={}
+colors=[0x000000,0xafafaf,0x12ce6f,0xffffff,0xab92e1,0xfa579e,0xe48ad1,0x32af6c,0xcf5cf5]
+
+conn=sqlite3.connect("tags.db")
+curs=conn.cursor()
+curs.execute("""CREATE TABLE IF NOT EXISTS tag(
+             tag_name text,
+             link text,
+             contributor int)""")
+
+def Tags():
+    curs.execute("SELECT * FROM tag")
+    info=curs.fetchall()
+    l=[]
     for i in info:
-        channel=i[1]
-        guild=i[0]
-        l=[]
-        channel=channel.split("-")
-        for z in channel:
-            if z=="":
-                pass
-            else:
-                l.append(int(z))
-        d[guild]=l
-    return d
+        l.append(i[0])
+    return l
 
 @bot.event
 async def on_ready():
-    print(f"The {bot.user} is ready!")
+    print(f"{bot.user} is online!")
     await bot.tree.sync()
 
-@bot.tree.command(name="choose")
-@app_commands.checks.has_permissions(manage_guild=True)
-async def choose(interaction:discord.Interaction,channel:str):
-    await interaction.response.defer(ephemeral=True)
-    cursor.execute("SELECT * FROM ids")
-    info=cursor.fetchall()
-    for i in info:
-        if i[0]==interaction.guild.id:
-            if channel.startswith("<#") and channel.endswith(">"):
-                channel=channel.rstrip(">")
-                channel=channel.lstrip("<#")
-                try:
-                    channel=int(channel)
-                    cursor.execute(f"UPDATE ids SET channel_id=? WHERE guild_id={interaction.guild.id}",[f"{str(channel)}-"])
-                    connect.commit()
-                    return await interaction.followup.send(f"The <#{channel}> has been assigned!")
-                except ValueError as e:
-                    print(e)
-                    return await interaction.followup.send("Please send a proper channel mention only")
-            else:
-                return await interaction.followup.send("Please send a proper channel mention only")
-        else:
-            print("Checking line 43")
-    if channel.startswith("<#") and channel.endswith(">"):
-        channel=channel.rstrip(">")
-        channel=channel.lstrip("<#")
-        try:
-            channel=int(channel)
-            cursor.execute("INSERT INTO ids VALUES (?,?)",[interaction.guild.id,f"{channel}-"])
-            connect.commit()
-            await interaction.followup.send(f"The <#{channel}> has been assigned!")
-            return
-        except ValueError as e:
-            print(e)
-            return await interaction.followup.send("Please send a proper channel mention only")
-    else:
-        return await interaction.followup.send("Please send a proper channel mention only")
+@bot.command()
+async def tag(ctx:commands.Context,tags:str,link:str):
+    if " " in link:
+        link=link.replace(" ","")
+    response=requests.get(link)
+    if response.status_code==200:
+        if "jpg" in link or "jpeg" in link or "png" in link or "webp" in link or "gif" in link:
+            curs.execute("INSERT INTO tag VALUES (?,?,?)",[tags,link,ctx.author.id])
+            conn.commit()
+            embed=discord.Embed(color=random.choice(colors),description=f"*Thanks for your contribution {ctx.author.mention}*\n*Tag: {tags}*")
+            embed.set_image(url=link)
+            embed.set_author(name=ctx.author.global_name,icon_url=ctx.author.display_avatar.url)
+            return await ctx.send(embed=embed)
+    await ctx.send("Invalid link :(\n-# Suggestion, look at the links which have words like jpg, jpeg, png, webp, gif, these extension supports images and no other extensions are valid here")
     
-@bot.event
-async def on_message(message:discord.Message):
-    dictionary=IDS()
-    if message.author==bot.user:
-        return
-    if message.guild.id in dictionary:
-        channels=dictionary[message.guild.id]
-        try:
-            channel=channels.index(message.channel.id)
-            if channel is None:
-                print("line 101")
-            else:
-                channel=message.guild.get_channel(channels[channel])
-                await channel.send(f"{random.choice(["meow","meow :3","meow ;3"])}")
-        except ValueError as e:
-            print(f"{e} but it's fine!")
-    await bot.process_commands(message)
+
+@bot.command()
+async def hugs(ctx:commands.Context,member:discord.Member|None=None):
+    curs.execute(f"SELECT * FROM tag WHERE tag_name=?",["hug"])
+    links=curs.fetchall()
+    img=random.choice(links)
+    if member is not None:
+        embed=discord.Embed(color=random.choice(colors),description=f"*Hugs you {member.mention}!!!*")
+        embed.set_image(url=img[1])
+        await ctx.send(embed=embed)
+    else:
+        embed=discord.Embed(color=random.choice(colors),description=f"*Hugs you {ctx.author.mention}*")
+        embed.set_image(url=img[1])
+        await ctx.send(embed=embed)
 
 bot.run("TOKEN")
 
